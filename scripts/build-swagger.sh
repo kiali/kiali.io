fe49b5bcb9c4e8074a5d84a48ea4a49445eda35c
Josejulio Martínez <jmartine@redhat.com>
2018-10-17 15:30:07 -0500

    KIALI-1697 Use the same swagger generator as kiali   go-swagger uses internally ReDoc to display the page

--- scripts/build-swagger.sh
+++ scripts/build-swagger.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 # Build Swagger API
-java -jar ./scripts/swagger2markup-cli-1.3.3.jar convert  -i https://raw.githubusercontent.com/kiali/kiali/master/swagger.json  -d ./output/content/api -c ./scripts/config.properties
 
-# Delete first link (useless and breaks Hugo since it is an HTML anchor and not proper Markdown)
-for f in output/content/api/*; do
-    echo "Processing $f"
-    sed '/## /,$!d' $f > content/api/`basename $f`
-done
+curl https://raw.githubusercontent.com/kiali/kiali/master/swagger.json > swagger.json
+npx redoc-cli bundle -t scripts/template.hbs swagger.json
+rm swagger.json
+mv redoc-static.html content/api/index.html
