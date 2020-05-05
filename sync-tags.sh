de53211812f71a577a24c2a09652ee980addd863
Cainã Costa <me@cfcosta.com>
2020-05-05 14:45:51 -0300

    Add proper filtering of the tags, fix problems with incomplete tags being marked.

--- sync-tags.sh
+++ sync-tags.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
+# This script does the migration process necessary for documentation
+# versioning. It is to be removed after that initiative is done.
+
+# Be careful running it as it creates tags on the current repo you're on.
+# Run with "-f" to disable dry run mode.
+
 KIALI_REPO="${KIALI_REPO:-${GOPATH}/src/github.com/kiali/kiali}"
-FIRST_NON_LEGACY_VERSION="v1.17.0"
-
-compare_dates() {
-  local date1=$(date -d "${1}" +%s)
-  local date2=$(date -d "${2}" +%s)
-
-  [ ${date1} -ge ${date2} ]
-}
+FIRST_LEGACY_VERSION="v1.0.0"
+LAST_LEGACY_VERSION="v1.17.0"
 
 # TODO: rename
 create_kiali_io_tags() {
   pushd ${KIALI_REPO} &>/dev/null
 
-  # In order:
   # - List all versions in semver order
   # - Remove old versions that were tagged incorrectly (like "0.2.0")
   # - Remove snapshot versions
@@ -24,31 +23,35 @@ create_kiali_io_tags() {
   # Removing those versions from the list is safe because we are only going to
   # iterate over legacy tags. New tags are supposed to be created on version
   # release.
-  local versions=$(git tag -l --sort=v:refname \
-    | egrep -v "^[^v]" \
-    | grep -v -- "-snapshot" \
-    | egrep -v "\\.[^0-9]+$" \
-    | egrep -v "\\.[^0]$")
-  local start_date=$(git show -s --format="%ci" ${version} | cut -f 1 -d" ")
+  local versions=$(git tag -l --sort=v:refname | egrep "v[0-9]+\\.[0-9]+\\.0$")
+  local start_date=$(git show -s --format="%at" ${FIRST_LEGACY_VERSION})
+  local end_date=$(git show -s --format="%at" ${LAST_LEGACY_VERSION})
 
   for version in ${versions}; do
-    local commit_date=$(git show -s --format="%ci" ${version})
+    local commit_date=$(git show -s --format="%at" ${version})
 
-    if compare_dates ${start_date} ${commit_date}; then
-      popd &>/dev/null
+    [ "${commit_date}" -lt "${start_date}" ] && continue
+    [ "${commit_date}" -gt "${end_date}" ]  && continue
 
-      local latest_commit_on_tag=$(git rev-list -1 --before=$(date +%Y-%m-%d -d "${commit_date} - 1 minute") --format="%ci" master \
-        | grep commit | sed -e "s/commit //" \
-        | tail -n 1)
+    popd &>/dev/null
 
-      echo "Creating tag for version ${version}"
+    # Important: here we only fetch merge commits, so we don't point at incomplete work.
+    local latest_commit_on_tag=$(git rev-list -1 --merges --before=$(date +%s -d "@$(echo "${commit_date} - 1" | bc)") --format="%at" master \
+      | grep commit | sed -e "s/commit //" \
+      | tail -n 1)
+
+    echo "Creating tag for version ${version}..."
+
+    if [ "${1}" = "-f" ]; then
       git tag ${version} ${latest_commit_on_tag} || echo "Version ${version} already exists"
-
-      pushd ${KIALI_REPO} &>/dev/null
+    else
+      echo "git tag ${version} ${latest_commit_on_tag}"
     fi
+
+    pushd ${KIALI_REPO} &>/dev/null
   done
 
   popd &>/dev/null
 }
 
-create_kiali_io_tags
+create_kiali_io_tags ${1}
