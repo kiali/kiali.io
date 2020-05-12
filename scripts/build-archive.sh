16689cb4e84979cff2997d6fe713ac56c57fe99b
Cainã Costa <me@cfcosta.com>
2020-05-12 13:19:12 -0300

    Only generate for one version

--- scripts/build-archive.sh
+++ scripts/build-archive.sh
@@ -34,7 +34,7 @@ info() {
   echo "$(color 2 "$(log_prefix)") ${@}"
 }
 
-#[[ -z $(git status -s) ]] || die "The working tree is dirty. Please make sure it is clean before running."
+[[ -z $(git status -s) ]] || die "The working tree is dirty. Please make sure it is clean before running."
 
 set -e
 
@@ -53,8 +53,9 @@ rm -rf "${destination}/v*"
 info "Making sure we have all the latest tags..."
 git fetch --all --tags
 
-info "Running for versions: $(git tag --list | paste -sd "," -)"
-for tag in $(git tag --list); do
+generate() {
+  tag="${1}"
+
   info "Preparing documentation for version ${tag}"
   ${GIT} checkout ${tag}
   mkdir ${tempdir}/${tag}
@@ -67,7 +68,22 @@ for tag in $(git tag --list); do
   sed -i "s/Documentation/${tag}/g" "${tempdir}/${tag}/_index.adoc"
 
   ${GIT} checkout ${current_branch}
-done
+}
+
+if [ "${1}" = "all" ]; then
+  info "Running for versions: $(git tag --list | paste -sd "," -)"
+  for tag in $(git tag --list); do
+    generate ${tag}
+  done
+else
+  info "Running for version ${1}..."
+
+  if git tag --list | grep "${1}" &>/dev/null; then
+    generate ${1}
+  else
+    die "Tag ${1} could not be found"
+  fi
+fi
 
 cp -rf ${tempdir}/* ${destination}
 
