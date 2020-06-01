20ca3d801f551a804015d99e6c6c1b97f0a83852
Cainã Costa <me@cfcosta.com>
2020-06-01 16:07:56 -0300

    Remove backwards compatibility for the archive script and prepare for CI

--- scripts/build-archive.sh
+++ scripts/build-archive.sh
@@ -2,43 +2,13 @@
 
 set -xe
 
-[[ -z $(git status -s) ]] || (echo "The working tree is dirty. Please make sure it is clean before running."; exit 1)
+tag="${1%.0}"
+root="$(pwd)/content/documentation"
 
-GIT="git -c advice.detachedHead=false"
+echo "Preparing documentation for version ${tag}"
 
-current_branch=$(git rev-parse --abbrev-ref HEAD)
-trap '{ ${GIT} checkout ${current_branch}; }' EXIT
+cp -r "${root}"/edge "${root}"/${tag}
 
-echo "Making sure we have all the latest tags..."
-git fetch --all --tags
-
-temp="$(mktemp -d)"
-
-generate() {
-  tag="${1%.0}"
-  root="$(pwd)/content/documentation"
-
-  echo "Preparing documentation for version ${tag}"
-
-  ${GIT} checkout "${1}"
-  mkdir "${temp}/${tag}"
-
-  cp -r "${root}"/edge/* "${temp}/${tag}/"
-
-  find "${temp}/${tag}" -type f -name "*.adoc" -exec sed -i -e "s/  main:/  $(echo "${tag}" | tr "." "-"):/" "{}" \;
-  sed -i "s/: (Edge|Documentation)/: ${tag}/g" "${temp}/${tag}/_index.adoc"
-
-  ${GIT} checkout "${current_branch}"
-
-  mkdir "${root}/${tag}"
-  cp -r "${temp}/${tag}/*" "${root}/"
-}
-
-echo "Running for version ${1}..."
-
-if git tag --list | grep "${1}" &>/dev/null; then
-  generate "${1}"
-else
-  echo "Tag ${1} could not be found"
-  exit 1
-fi
+find "${root}/${tag}" -type f -name "*.adoc" -exec sed -i -e "s/  main:/  $(echo "${tag}" | tr "." "-"):/" "{}" \;
+find "${root}/${tag}" -type f -name "*.adoc" -exec sed -i -e "s;documentation/edge;documentation/${tag};g" "{}" \;
+sed -i -r "s/: \"(Edge|Documentation)\"/: ${tag}/g" "${root}/${tag}/_index.adoc"
