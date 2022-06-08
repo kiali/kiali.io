068c858f211c33301e296439b0cbe66453f9748b
josunect <josune.cordoba@gmail.com>
2022-06-08 08:48:08 +0100

    Clean script ignore new urls

--- scripts/ignore_new_urls.sh
+++ scripts/ignore_new_urls.sh
@@ -5,23 +5,17 @@
 
 main() {
   MERGEBASE=$(git merge-base origin/staging HEAD | sed 's/ //g')
-  #echo $MERGEBASE
 
   SUB='content/en/docs'
   EXCLUDE_URLS=""
 
   while IFS= read -r line; do
   if [[ "$line" =~ .*"$SUB".* ]]; then
-              #printf "$line\n"
+
               SUBSTR=$(echo $line | sed 's,content/en/docs/,,g')
-              #printf "$SUBSTR\n"
-              #SUBSTR=$(echo $SUBSTR | sed 's,/,/\\,g')
-              #printf "$SUBSTR\n"
               PATTERN=$(echo "${SUBSTR%.*}")
-              #printf "$PATTERN\n"
               URL=$(echo ",/.*${SUBSTR%.*}.*/")
               URL=$(echo "$URL" | awk '{print tolower($0)}')
-              #printf "$URL\n"
               EXCLUDE_URLS="$EXCLUDE_URLS$URL"
       fi;
   done <<< "$(git diff --name-only --diff-filter=ACR $MERGEBASE)"
